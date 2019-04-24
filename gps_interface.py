93138f30106d6f9f54f3142b347aefc8f1141e25
Stephen McDonnell <me@stephen.com>
2019-04-24 01:30:36 +0100

    GPS update + test 3

--- gps_interface.py
+++ gps_interface.py
@@ -1,109 +1,64 @@
 # Stephen McDonnell
 # 16/04/2019
 
-# This File consists of API calls for the GPS Parsing Functionality.This section parse the GPS values from the GPS Module.
-# pynmea2 is Python library for parsing the NMEA 0183 protocol (GPS)
-
-#import random
+import os
 from gps import *
+from time import *
 import time
 import threading
-import pynmea2
-#import os
 
-gpsd = None
+gpsd = None  # seting the global variable
 
-class GPSInterface():
 
+
+class GPSInterface(threading.Thread):
     def __init__(self):
-        self.latitude = None
-        self.longitude = None
-        self.msg = ''
+        threading.Thread.__init__(self)
+        global gpsd  # bring it in scope
+        gpsd = gps(mode=WATCH_ENABLE)  # starting the stream of info
+        self.current_value = None
+        self.running = True  # setting the thread running to true
+#
+    def run(self):
+        global gpsd
+        while gpsp.running:
+            gpsd.next()  # this will continue to loop and grab EACH set of gpsd info to clear the buffer
 
-    # Gps Receiver thread function, check gps value for infinite times
-    def getgpsdata(serial, dmesg):
-        print("getgpsdata")
+
+if __name__ == '__main__':
+    gpsp = GPSInterface()  # create the thread
+    try:
+        gpsp.start()  # start it up
         while True:
-            data = serial.readline()
-            if data.find('GGA') > 0:
-                dmesg.msg = pynmea2.parse(data)
+            # It may take a second or two to get good data
+            # print gpsd.fix.latitude,', ',gpsd.fix.longitude,'  Time: ',gpsd.utc
 
-    # API to call start the GPS Receiver
-    def start_gps_receiver(serial, dmesg):
-        t2 = threading.Thread(target=serial.getgpsdata, args=(serial, dmesg))
-        t2.start()
-        print("GPS Receiver started")
+            os.system('clear')
 
-    # API to fix the GPS Revceiver
-    def ready_gps_receiver(msg):
-        print("Please wait fixing GPS .....")
-        dmesg = msg.msg
-        while (dmesg.gps_qual != 1):
-            pass
-        print("GPS Fix available")
+            print
+            print(' GPS reading')
+            print('----------------------------------------')
+            print('latitude    ', gpsd.fix.latitude)
+            print('longitude   ', gpsd.fix.longitude)
+            print('time utc    ', gpsd.utc, ' + ', gpsd.fix.time)
+            print('altitude (m)', gpsd.fix.altitude)
+            print('eps         ', gpsd.fix.eps)
+            print('epx         ', gpsd.fix.epx)
+            print('epv         ', gpsd.fix.epv)
+            print('ept         ', gpsd.fix.ept)
+            print('speed (m/s) ', gpsd.fix.speed)
+            print('climb       ', gpsd.fix.climb)
+            print('track       ', gpsd.fix.track)
+            print('mode        ', gpsd.fix.mode)
+            print
+            print('sats        ', gpsd.satellites)
 
-    # API to get latitude from the GPS Receiver
-    def get_latitude(msg):
-        print("Getting Latitude")
-        dmesg = msg.msg
-        print("Latitude:", dmesg.latitude)
+            time.sleep(5)  # set to whatever
 
-    # API to get longitude from the GPS Receiver
-    def get_longitude(msg):
-        print("Getting Longitude")
-        dmesg = msg.msg
-        print("Longitude:", dmesg.longitude)
-
-    # API to get Number of Satellites from the GPS Receiver
-    def get_num_satellite(msg):
-        print("Getting Number of satellite")
-        dmesg = msg.msg
-        print("No of satellites:", dmesg.num_sats)
-
-    # API to get Altitude of Antenna from the GPS Receiver
-    def get_altitude(msg):
-        print("Getting altitude of Antenna")
-        dmesg = msg.msg
-        print("Altitude (M):", dmesg.altitude)
-
-    # API to check the status of GPS Fix
-    def get_gps_fix(msg):
-        dmesg = msg.msg
-        print("GPS Fix stats:", dmesg.gps_qual)
-
-    # API to Exit
-    def function_exit(msg):
-        print("Exiting ......")
-        print("stopping the thread")
-        exit()
-        return 1
-
-    def get_gps_reading(self):
-# Python Hardware calls here i.e. read UART and get data
-
-        # Sudo number gen for testing
-        #random.seed(time.time())
-        #self.latitute = round(random.randint(-900, 900) * 0.10, 2)
-        #self.longitude = round(random.randint(-1800, 1800) * 0.10)
-
-        self.latitute = self.get_latitude()
-        self.longitude = self.get_longitude()
-        return self.latitute, self.longitude
-
-
-##################################################################################
-#########################            TEST            #############################
-#    def __init__(self):
-#        self.latitute = None
-#        self.longitude = None
-#
-#    def get_gps_reading(self):
-#        # Python Hardware calls here i.e. read UART and get data
-#
-#        # Sudo number gen for testing
-#        random.seed(time.time())
-#        self.latitute = round(random.randint(-900, 900) * 0.10, 2)
-#        self.longitude = round(random.randint(-1800, 1800) * 0.10)
-#        return self.latitute, self.longitude
-##################################################################################
-##################################################################################
+    except (KeyboardInterrupt, SystemExit):  # when you press ctrl+c
+        print
+        "\nKilling Thread..."
+        gpsp.running = False
+        gpsp.join()  # wait for the thread to finish what it's doing
+    print
+    "Done.\nExiting."
