51f86a8ec96bcf542d46ddd962f4bcecc6da89fc
Stephen McDonnell <me@stephen.com>
2019-04-24 01:34:44 +0100

    GPS update + test 4

--- gps_interface.py
+++ gps_interface.py
@@ -1,64 +1,42 @@
 # Stephen McDonnell
 # 16/04/2019
 
-import os
 from gps import *
 from time import *
 import time
-import threading
+from multiprocessing import Process, Queue
 
-gpsd = None  # seting the global variable
+class GPSInterface(Process):
 
-
-
-class GPSInterface(threading.Thread):
-    def __init__(self):
-        threading.Thread.__init__(self)
-        global gpsd  # bring it in scope
-        gpsd = gps(mode=WATCH_ENABLE)  # starting the stream of info
+    def __init__(self, queue):
+        super().__init__()
+        self.gpsd = None # bring it in scope
+        self.gpsd = gps(mode=WATCH_ENABLE)  # starting the stream of info
         self.current_value = None
-        self.running = True  # setting the thread running to true
-#
+        self.queue = queue
+        self.latitude = None
+        self.longitude = None
+
     def run(self):
-        global gpsd
-        while gpsp.running:
-            gpsd.next()  # this will continue to loop and grab EACH set of gpsd info to clear the buffer
+        while True:
+            self.gpsd.next()  # this will continue to loop and grab EACH set of gpsd info to clear the buffer
+            self.get_gps_reading()
+            time.sleep(1)
+
+    def get_gps_reading(self):
+        self.latitude = self.gpsd.fix.latitude
+        self.longitude = self.gpsd.fix.longitude
+        self.queue.put((self.latitude, self.longitude))
 
 
 if __name__ == '__main__':
-    gpsp = GPSInterface()  # create the thread
-    try:
-        gpsp.start()  # start it up
-        while True:
-            # It may take a second or two to get good data
-            # print gpsd.fix.latitude,', ',gpsd.fix.longitude,'  Time: ',gpsd.utc
-
-            os.system('clear')
-
-            print
-            print(' GPS reading')
-            print('----------------------------------------')
-            print('latitude    ', gpsd.fix.latitude)
-            print('longitude   ', gpsd.fix.longitude)
-            print('time utc    ', gpsd.utc, ' + ', gpsd.fix.time)
-            print('altitude (m)', gpsd.fix.altitude)
-            print('eps         ', gpsd.fix.eps)
-            print('epx         ', gpsd.fix.epx)
-            print('epv         ', gpsd.fix.epv)
-            print('ept         ', gpsd.fix.ept)
-            print('speed (m/s) ', gpsd.fix.speed)
-            print('climb       ', gpsd.fix.climb)
-            print('track       ', gpsd.fix.track)
-            print('mode        ', gpsd.fix.mode)
-            print
-            print('sats        ', gpsd.satellites)
-
-            time.sleep(5)  # set to whatever
-
-    except (KeyboardInterrupt, SystemExit):  # when you press ctrl+c
-        print
-        "\nKilling Thread..."
-        gpsp.running = False
-        gpsp.join()  # wait for the thread to finish what it's doing
-    print
-    "Done.\nExiting."
+    queue = Queue()
+    gps_interface = GPSInterface(queue)
+    gps_interface.daemon = True
+    gps_interface.start()
+    while True:
+        try:
+            print(queue.get(block=False))
+        except Exception as e:
+            pass
+        time.sleep(0.5)
